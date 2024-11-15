35418bf892b4e15be41e5d1e701a1993c857a07b
Andrey Agarkov <business.yavorprod@gmail.com>
2024-11-15 18:51:18 +0300

    upd - admin panel & subscriptions logic optimization

--- backend/apps/vending/models/tariff.py
+++ backend/apps/vending/models/tariff.py
@@ -1,11 +1,16 @@
-from django.db.models import Model, CharField, PositiveIntegerField
+from django.db.models import Model, CharField, PositiveIntegerField, BooleanField
 
 
 class SubscriptionTariff(Model):
-    name = CharField("Описание", max_length=100)
+    is_active = BooleanField("Активен", default=True)
+    name = CharField("Название тарифа", max_length=100)
     days = PositiveIntegerField("Кол-во дней")
     cups = PositiveIntegerField("Кол-во чашек в день")
     price = PositiveIntegerField("Цена (в рублях)")
+
+    class Meta:
+        verbose_name = "тариф"
+        verbose_name_plural = "Тарифы абонементов"
 
     def __str__(self):
         return self.name
