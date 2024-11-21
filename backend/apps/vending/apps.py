1d7de6cb5692fd33e61a0333915117820cc24930
Andrey Agarkov <business.yavorprod@gmail.com>
2024-11-21 15:09:35 +0300

    upd - rename vending app

--- backend/apps/vending/apps.py
+++ backend/apps/vending/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class VendingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.vending"
-    verbose_name = "Вендинг"
+    verbose_name = "Точка Чёрного"
 
     def ready(self):
         import apps.vending.signals
